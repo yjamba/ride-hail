95f979a05ec062c29d1f5c841ab70678e08b8b55
zhyechsha <yjamba@icloud.com>
2026-01-17 11:01:56 +0500

    fix(middleware): implement authMiddleware

--- internal/ride/handlers/middleware/middleware.go
+++ internal/ride/handlers/middleware/middleware.go
@@ -1,17 +1,21 @@
 package middleware
 
-import (
-	"net/http"
-	"strings"
-)
+import "net/http"
 
-func JsonMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ct := r.Header.Get("Content-Type")
-		if ct == "" || !strings.HasPrefix(ct, "application/json") {
-			http.Error(w, "Content-Type must be application/json", http.StatusUnsupportedMediaType)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+type MiddlewareChain struct {
+	middlewares []func(next http.HandlerFunc) http.HandlerFunc
+}
+
+func NewMiddlewareChain(middlewares ...func(next http.HandlerFunc) http.HandlerFunc) *MiddlewareChain {
+	return &MiddlewareChain{
+		middlewares: middlewares,
+	}
+}
+
+func (mc *MiddlewareChain) WrapHandler(handler http.HandlerFunc) http.HandlerFunc {
+	for i := len(mc.middlewares) - 1; i >= 0; i-- {
+		handler = mc.middlewares[i](handler)
+	}
+
+	return handler
 }
